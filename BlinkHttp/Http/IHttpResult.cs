392453d024269c1ac172103c33fbe2c7b16e7e7e
Ether141 <42118307+Ether141@users.noreply.github.com>
2025-05-03 19:18:17 +0200

    Add general type for HTTP result

--- BlinkHttp/Http/IHttpResult.cs
+++ BlinkHttp/Http/IHttpResult.cs
@@ -7,8 +7,23 @@ namespace BlinkHttp.Http;
 /// </summary>
 public interface IHttpResult
 {
+    /// <summary>
+    /// Gets the raw data of the HTTP response.
+    /// </summary>
     byte[] Data { get; }
+
+    /// <summary>
+    /// Gets the content type of the HTTP response, typically a MIME type (e.g., "application/json").
+    /// </summary>
     string? ContentType { get; }
+
+    /// <summary>
+    /// Gets the content disposition of the HTTP response, which can indicate if the content is inline or an attachment.
+    /// </summary>
     string? ContentDisposition { get; }
+
+    /// <summary>
+    /// Gets or sets the HTTP status code of the response.
+    /// </summary>
     HttpStatusCode HttpCode { get; set; }
 }
